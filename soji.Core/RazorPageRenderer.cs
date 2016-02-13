e10694689b47d87a3b9cce50e27aa66104bf1b2b
Kenichi Nakamura <nkkn1008@gmail.com>
2016-02-13 19:57:36 +0900

    Add soji.core library
    Test refer the project

---
+++ soji.Core/RazorPageRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soji.Core
+{
+    public class RazorPageRenderer
+    {        
+        public string Render()
+        {
+            return "Render Test";
+        }
+    }
+}
---
+++ soji.Test/RazorPageRendererTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace soji.Test
+{
+    public class RazorPageRendererTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            var test = new 
+            Assert.Equal(test.add(5, 4), 9);
+        }
+    
+    }
+}
