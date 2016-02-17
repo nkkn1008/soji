fe8b5001c5cbf35ff4cf03e2f280a390e7c50c86
Kenichi Nakamura <nkkn1008@gmail.com>
2016-02-17 19:49:43 +0900

    Render ボタンを押下により RazorEngineを利用して作成された文字列がfile書き込みできるようにした
    内容と、出力ファイル名は固定

--- soji/ViewModel/MainWindowViewModel.cs
+++ soji/ViewModel/MainWindowViewModel.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using soji.Helpers;
 using System.Windows;
+using soji.Core;
+using System.IO;
 
 namespace soji.ViewModel
 {
     public class MainWindowViewModel
     {
         public RelayCommand RenderCommand { get; set; }
-
+        
         public MainWindowViewModel()
         {
             RenderCommand = new RelayCommand(RenderPage);
@@ -19,7 +21,23 @@ namespace soji.ViewModel
 
         void RenderPage(object selectedItem)
         {
-            MessageBox.Show("hoge");
+            Series series = new Series();
+            Target target1 = new Target { Name = "TargetA" };            
+            List<Target> targets = new List<Target>();
+            targets.Add(target1);
+
+            RazorModel model = new RazorModel();
+            model.Series = series;
+            model.Targets = targets;
+
+            string template = @"Series code is @Model.Series.Code";
+            model.Series.Code = "37";
+            var renderer = new RazorPageRenderer(template, model);
+            string result = renderer.Render();
+            using (var writer = new StreamWriter(@"C:\work\test.txt", false, System.Text.Encoding.UTF8))
+            {
+                writer.WriteLine(result);
+            }
         }
     }
 }
