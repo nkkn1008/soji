948515d8e36c4ddce9a81afe6e02512f41f7e7f5
Kenichi Nakamura <nkkn1008@gmail.com>
2016-02-17 19:41:51 +0900

    MainWindow用の ViewModelを追加

--- soji/MainWindow.xaml.cs
+++ soji/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using soji.ViewModel;
 
 namespace soji
 {
@@ -23,6 +24,8 @@ namespace soji
         public MainWindow()
         {
             InitializeComponent();
+            var p = new MainWindowViewModel();
+            this.DataContext = p;
         }
 
         private void TextBox_DragEnter(object sender, DragEventArgs e)
---
+++ soji/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using soji.Helpers;
+using System.Windows;
+
+namespace soji.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public RelayCommand RenderCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            RenderCommand = new RelayCommand(RenderPage);
+        }
+
+        void RenderPage(object selectedItem)
+        {
+            MessageBox.Show("hoge");
+        }
+    }
+}
