7475c6cf36c1ed83d3c8c64e65b8147b9bec28ec
Kenichi Nakamura <nkkn1008@gmail.com>
2016-02-20 08:57:17 +0900

    :white_check_mark: ModelCongiuratorのテストを追加

---
+++ soji.Test/ModelConfiguratorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using soji.Core;
+
+namespace soji.Test
+{
+    public class ModelConfiguratorTest
+    {
+        [Fact]
+        public void MapJsonToRazorModel()
+        {
+            string json = @"{
+  'Targets':
+  [
+      {
+         'Name': 'Target-A'
+      }
+       ,
+      {
+          'Name': 'Target-B'
+      }
+  ],
+  'Series': 
+  {                
+    'Code': '9999'
+  }
+        }";
+            ModelConfigurator configurator = new ModelConfigurator(json);
+            RazorModel model = configurator.Run();
+            Assert.Equal("9999",model.Series.Code);
+        }
+    }
+}
